593b28f9a5fb5e9fd390923c44212c2c1cfb808f
khajamoinuddin26 <khajamoinuddinsameer@gmail.com>
2024-04-08 14:50:27 +0530

    dp changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import Dp from "../assets/dp.jpg"
+import Dp from "../assets/sameerk.jpg"
 import { MdOutlineArrowRightAlt } from 'react-icons/md'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
