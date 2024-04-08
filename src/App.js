8ce31ed4e64fb3ad1184ee7fd13986841ee82b61
khajamoinuddin26 <khajamoinuddinsameer@gmail.com>
2024-04-08 15:13:09 +0530

    speed insight

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Projects from './components/Projects';
 import SocialLinks from './components/SocialLinks';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 
 function App() {
