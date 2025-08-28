9c014f5d68516b13d4bddb847a493d5de3ffbdee
khajamoinuddin26 <khajamoinuddinsameer@gmail.com>
2025-08-28 23:41:05 +0530

    fix:syntax

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,13 @@
 import { EarthCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from "./Hero";
-import Navbar from "./Navbar";
-import About from "./About";
-import Tech from "./Tech";
-import Experience from "./Experience";
-import Works from "./Works";
-import Feedbacks from "./Feedbacks";
-import Contact from "./Contact";
-import CanvasLoader from "./Loader";
+import Hero from "./Hero.jsx";
+import Navbar from "./Navbar.jsx";
+import About from "./About.jsx";
+import Tech from "./Tech.jsx";
+import Experience from "./Experience.jsx";
+import Works from "./Works.jsx";
+import Feedbacks from "./Feedbacks.jsx";
+import Contact from "./Contact.jsx";
+import CanvasLoader from "./Loader.jsx";
 
 
 export {
